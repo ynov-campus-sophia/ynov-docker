81819d36523b8d27cfabbcbe0317bb5695e0c319
Aureliano Sinatra <aureliano.buendias@outlook.com>
2022-11-21 16:17:55 +0100

    cleanup

--- docker/compose/datacollect-inov/src/base.py
+++ docker/compose/datacollect-inov/src/base.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-connection = 'postgresql://{}:{}@{}:{}/{}'.format(os.getenv('user', 'executor'), os.getenv('password', '123456'),os.getenv('host', 'localhost'),os.getenv('port', 5432),os.getenv('database', 'crypto-executor'))
+connection = 'postgresql://{}:{}@{}:{}/{}'.format(os.getenv('DB_USER', 'executor'), os.getenv('DB_PASSWORD', '123456'),os.getenv('DB_HOST', 'pg'),os.getenv('DB_PORT', 5432),os.getenv('DB_NAME', 'crypto-executor'))
 engine = create_engine(connection)
 Session = sessionmaker(bind=engine)
 
